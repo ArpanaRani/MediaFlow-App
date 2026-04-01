163181a42ea0bf0b0775568e01dd45733c3e1009
Arpana Rani <arpanaaims@gmail.com>
2026-04-01 18:22:55 +0530

    Initial Commit

---
+++ MediaFlow/MediaFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediaFlowApp.swift
+//  MediaFlow
+//
+//  Created by Arpana Rani on 01/04/26.
+//
+
+import SwiftUI
+
+@main
+struct MediaFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MediaFlowTests/MediaFlowTests.swift
@@ -0,0 +1,17 @@
+//
+//  MediaFlowTests.swift
+//  MediaFlowTests
+//
+//  Created by Arpana Rani on 01/04/26.
+//
+
+import Testing
+@testable import MediaFlow
+
+struct MediaFlowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
